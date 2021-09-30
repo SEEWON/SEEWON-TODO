adf05bc4b571973fb02116bb82ad9dc953bbcf35
SEEWON <andyaaa78@gmail.com>
2021-09-30 14:49:24 +0900

    [fix] Fix bug items having wrong ID

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 import TodoList from './TodoList';
-import { useState } from 'react';
+import { React, useState } from 'react';
 
 function App() {
-  let todoCnt = 3;
   const [inputValue, setInputValue] = useState('');
-  const [todos, setTodos] = useState([
-    { id: 1, text: '밥먹기', isDone: false },
-    { id: 3, text: '물마시기', isDone: false },
-  ]);
-  const [dones, setDones] = useState([
-    { id: 2, text: '숨쉬기', isDone: true },
-    { id: 4, text: '잠자기', isDone: true },
-  ]);
+  const [todos, setTodos] = useState([]);
+  const [dones, setDones] = useState([]);
+  const [todoCnt, setTodoCnt] = useState(0);
 
   const onChange = (event) => {
     const {
@@ -23,7 +17,7 @@ function App() {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    todoCnt++;
+    setTodoCnt(todoCnt + 1);
     setTodos([...todos, { id: todoCnt, text: inputValue, isDone: false }]);
     setInputValue('');
   };
@@ -55,6 +49,7 @@ function App() {
       <form onSubmit={onSubmit}>
         <input
           type="text"
+          value={inputValue}
           placeholder="할 일 추가하기"
           maxlength={20}
           onChange={onChange}
